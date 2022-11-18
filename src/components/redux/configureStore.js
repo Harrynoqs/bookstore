9093e2ffdf666fdf8bea94f817f81a757c99cd8e
Harrynoqs <noqsnoqs@gmail.com>
2022-11-18 14:26:30 +0300

    Removed linters

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import handleBook from './books/book';
-import handleStatus from './categories/categories';
+import handleStatus from './ categories/categories';
 
 const reducers = combineReducers({
   handleBook,
   handleStatus,
 });
+
 const store = configureStore({
   reducer: reducers,
 });
+
+export default store;
